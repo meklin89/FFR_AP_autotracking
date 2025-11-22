a93d15815386b88f8dfac55ba9a8e28c8e479066
meklin89 <meklin89@gmail.com>
2025-11-22 00:16:54 -0600

    Add dwarf locations to loadLocations script

--- scripts/loadLocations.lua
+++ scripts/loadLocations.lua
@@ -1,3 +1,4 @@
 Tracker:AddLocations("locations/incentives.json")
 Tracker:AddLocations("locations/overworld.json")
 Tracker:AddLocations("locations/ToF.json")
+Tracker:AddLocations("locations/dwarf.json")
